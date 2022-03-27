aad1a6ecc69ca2b5d31cc7e2010bf2b07d9930a6
Vitali Shukevich <v.shukevich@iomico.com>
2022-03-27 16:16:07 +0300

    lesson1: создание MainViewModel

--- app/src/main/java/com/shukevich/shoppinglist/preaentation/MainActivity.kt
+++ app/src/main/java/com/shukevich/shoppinglist/preaentation/MainActivity.kt
@@ -2,11 +2,23 @@ package com.shukevich.shoppinglist.preaentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.shukevich.shoppinglist.R
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.shopList.observe(this){
+            Log.d("MainActivityTest", it.toString())
+        }
+        viewModel.getShopList()
     }
 }
---
+++ app/src/main/java/com/shukevich/shoppinglist/preaentation/MainViewModel.kt
@@ -0,0 +1,36 @@
+package com.shukevich.shoppinglist.preaentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.shukevich.shoppinglist.data.ShopListRepositoryImpl  // не должно быть дата слоя
+import com.shukevich.shoppinglist.domain.DeleteShopItemUseCase
+import com.shukevich.shoppinglist.domain.EditShopItemUseCase
+import com.shukevich.shoppinglist.domain.GetShopListUseCase
+import com.shukevich.shoppinglist.domain.ShopItem
+
+class MainViewModel: ViewModel(){
+
+    private val repository = ShopListRepositoryImpl // неправильно, презентейшен слой зависит от дата слоя, так нельзя
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private  val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = MutableLiveData<List<ShopItem>>()
+
+    fun getShopList(){  // получаем список элементов из UseCase
+        val list = getShopListUseCase.getShopList()
+        shopList.value = list
+    }
+
+    fun deleteShopItem(shopItem: ShopItem){          // удаляем итем
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+        getShopList()                                  // обнавляем лист
+    }
+
+    fun changeEnableState(shopItem: ShopItem){
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newItem)
+        getShopList()
+    }
+}
