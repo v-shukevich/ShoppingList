3eebbb8d05e87f9e9992f95a23638862747f6478
Vitali Shukevich <v.shukevich@iomico.com>
2022-03-28 19:30:16 +0300

    lesson2:  RecyclerView удаление свайпом

--- app/src/main/java/com/shukevich/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/shukevich/shoppinglist/presentation/ShopListAdapter.kt
@@ -1,5 +1,6 @@
 package com.shukevich.shoppinglist.presentation
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,14 +9,18 @@ import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.shukevich.shoppinglist.R
 import com.shukevich.shoppinglist.domain.ShopItem
+import java.lang.RuntimeException
 
 class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
-
+    var count = 0
     var shopList = listOf<ShopItem>()
         set(value) {
             field = value
             notifyDataSetChanged()
         }
+
+    var onShopItemLongClickListener:((ShopItem)  -> Unit)?= null
+    var onShopItemClickListener:((ShopItem)  -> Unit)?= null
 
     class ShopItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         val tvName: TextView = view.findViewById(R.id.tv_name)
@@ -23,30 +28,32 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(
-            R.layout.item_shop_disabled,
-            parent,
-            false
-        )
+        Log.d("ShopListAdapter", "onCreateView, count: ${count++}")
+        val layout = when(viewType){
+            VIEW_TYPE_DISABLED -> R.layout.item_shop_disabled
+            VIEW_TYPE_ENABLED  -> R.layout.item_shop_enabled
+            else ->throw RuntimeException("Unknow view type:$viewType")
+        }
+        val view = LayoutInflater.from(parent.context).inflate(layout, parent, false)
         return ShopItemViewHolder(view)
     }
 
     override fun onBindViewHolder(viewHolder: ShopItemViewHolder, position: Int) {
         val shopItem = shopList[position]
-        val status = if (shopItem.enabled){
-            "Active"
-        }else{
-            "Not Active"
-        }
 
         viewHolder.view.setOnLongClickListener {
+            onShopItemLongClickListener?.invoke(shopItem)
             true
         }
-        if(shopItem.enabled){
-            viewHolder.tvName.text = "${shopItem.name} $status"
-            viewHolder.tvCount.text = shopItem.count.toString()
-            viewHolder.tvName.setTextColor(ContextCompat.getColor(viewHolder.view.context,android.R.color.holo_red_dark))
+
+        viewHolder.view.setOnClickListener{
+            onShopItemClickListener?.invoke(shopItem)
         }
+
+        viewHolder.tvName.text = shopItem.name
+        viewHolder.tvCount.text = shopItem.count.toString()
+
+
     }
 
     // установка значения по умалчанию при переиспользовании вьюхолдера
@@ -54,10 +61,37 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
         super.onViewRecycled(viewHolder)
         viewHolder.tvName.text = ""
         viewHolder.tvCount.text = ""
-        viewHolder.tvName.setTextColor(ContextCompat.getColor(viewHolder.view.context,android.R.color.white))
+        viewHolder.tvName.setTextColor(
+            ContextCompat.getColor(
+                viewHolder.view.context,
+                android.R.color.white
+            )
+        )
     }
 
     override fun getItemCount(): Int {
         return shopList.size
+    }
+
+    // определение view по позиции элемента
+    override fun getItemViewType(position: Int): Int {
+        val item =shopList[position]
+        return if (item.enabled){
+            VIEW_TYPE_ENABLED
+        }else{
+            VIEW_TYPE_DISABLED
+        }
+    }
+
+
+    companion object{
+        const val  VIEW_TYPE_ENABLED = 100
+        const val  VIEW_TYPE_DISABLED = 101
+        const val MAX_POOL_SIZE = 5
+    }
+
+
+    interface OnShopItemLongClickListener {
+        fun onShopItemLongClick(shopItem: ShopItem)
     }
 }
