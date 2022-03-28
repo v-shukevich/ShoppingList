4bd791d48705a0abf26e8baf87cb1eb0d5fc9e64
Vitali Shukevich <v.shukevich@iomico.com>
2022-03-28 23:44:32 +0300

    lesson5: add ShopItemViewModel

--- app/src/main/java/com/shukevich/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/shukevich/shoppinglist/presentation/ShopItemActivity.kt
@@ -2,6 +2,7 @@ package com.shukevich.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shukevich.shoppinglist.R
 
 class ShopItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/shukevich/shoppinglist/presentation/ShopItemViewModel.kt
@@ -0,0 +1,65 @@
+package com.shukevich.shoppinglist.presentation
+
+import androidx.lifecycle.ViewModel
+import com.shukevich.shoppinglist.data.ShopListRepositoryImpl
+import com.shukevich.shoppinglist.domain.AddShopItemUseCase
+import com.shukevich.shoppinglist.domain.EditShopItemUseCase
+import com.shukevich.shoppinglist.domain.GetShopItemUseCase
+import com.shukevich.shoppinglist.domain.ShopItem
+import java.lang.Exception
+
+class ShopItemViewModel : ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+    private val getShopItemUseCase = GetShopItemUseCase(repository)
+    private val addShopItemUseCase = AddShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    fun getShopItem(shopItemId: Int) {
+        val item = getShopItemUseCase.getShopItem(shopItemId)
+    }
+
+    fun addShopItem(inputName: String?, inputCount: String?) {
+        val name = parseName(inputName)
+        val count = parseCount(inputCount)
+        val fieldsValid = validateInput(name,count)
+        if (fieldsValid) {
+            val shopItem = ShopItem(name,count,true)
+            addShopItemUseCase.addShopItem(shopItem)
+        }
+    }
+
+    fun editShopItem(inputName: String?, inputCount: String?) {
+        val name = parseName(inputName)
+        val count = parseCount(inputCount)
+        val fieldsValid = validateInput(name,count)
+        if (fieldsValid) {
+            val shopItem = ShopItem(name,count,true)
+            editShopItemUseCase.editShopItem(shopItem)
+        }
+    }
+
+    private fun parseName(inputName: String?): String {
+        return inputName?.trim() ?: ""
+    }
+
+    private fun parseCount(inputCount: String?): Int {
+        return try {
+            inputCount?.trim()?.toInt() ?: 0
+        } catch (e: Exception) {
+            0
+        }
+    }
+
+    private fun validateInput(name: String, count: Int): Boolean {
+
+        var result = true
+            if(name.isBlank())  {
+                result = false //TODO: show error input name
+            }
+            if(count <= 0)  {
+                result = false //TODO: show error input count
+            }
+        return result
+    }
+}
